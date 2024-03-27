63d18e3cde8983d26ee110d2166c5307365c5eaa
soash <soash2002@gmail.com>
2024-03-27 15:17:42 +0600

    commit

--- project/settings.py
+++ project/settings.py
@@ -120,11 +120,9 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_DIRS = [BASE_DIR/'static']
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-# STATICFILES_DIRS = [BASE_DIR/'static']
-# STATIC_ROOT = BASE_DIR/'staticfiles'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media'
